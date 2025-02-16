fa6765724f57bc55dacf466736e63fe685f62d5c
Garc Onierre <onierregarc@gmail.com>
2025-02-16 14:23:07 +0300

    mongoURI ensured

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ import { Category } from "./types";
 
 export const constants = {
     categories: ["animals", "cars", "nature", "space"] as Category[],
+    REMOTE_MONGO_URI: process.env.REMOTE_MONGO_URI || "mongodb://192.236.195.254:27017/storytelling"
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 
 import app from './app';
+import { constants } from './constants';
 import { connectDB } from './database/connectDB';
 import addStory from './database/service/addStory';
 import { Category } from './types';
 import logger from './utils/logger';
 
 const PORT = process.env.PORT || 3000;
-const mongoURI = process.env.REMOTE_MONGO_URI || process.env.LOCAL_MONGO_URI || "";
+const mongoURI = constants.REMOTE_MONGO_URI
 app.listen(PORT, async () => {
 
     const funcName = "[STORYLAND-MAIN] "
@@ -16,7 +17,7 @@ app.listen(PORT, async () => {
         await connectDB(mongoURI);
 
         logger.info(funcName + `Connected to mongoDB: ${mongoURI}, Server running on port ${PORT}`)
-        
+
     } catch (error: any) {
         logger.info('Error: ' + error.message || error);
     }
